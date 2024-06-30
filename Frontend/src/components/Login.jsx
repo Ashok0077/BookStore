eaef4e191f8d25c3d10059258623528f13e054dd
Ashok0077 <vaishnavashok7297@gmail.com>
2024-06-30 13:47:43 +0530

    commit

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
       password: data.password,
     };
     await axios
-      .get("https://book-store-api-hazel.vercel.app/user/login", userInfo)
+      .get("/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
