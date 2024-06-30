b4fd9f3293ef7948f951309393b8f01b886dbaf7
Ashok0077 <vaishnavashok7297@gmail.com>
2024-06-30 20:45:51 +0530

    commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//      proxy :{
+//       '/api': 'https://book-store-api-hazel.vercel.app'
+//      }
+//   },
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-     proxy :{
-      '/api': 'https://book-store-api-hazel.vercel.app'
-     }
+    proxy: {
+      "/api": {
+        //target: "http://localhost:3000",
+         target: "https://book-store-api-hazel.vercel.app",
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
-})
+});
